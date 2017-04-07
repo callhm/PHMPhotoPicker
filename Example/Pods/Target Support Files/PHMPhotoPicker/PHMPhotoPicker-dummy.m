27435f325a8723c75061df13cba0b3e9ecc09152
PHM <251962881@qq.com>
2017-04-07 18:00:55 +0800

    添加新工程

---
+++ Example/Pods/Target Support Files/PHMPhotoPicker/PHMPhotoPicker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PHMPhotoPicker : NSObject
+@end
+@implementation PodsDummy_PHMPhotoPicker
+@end
---
+++ Example/Pods/Target Support Files/PHMPhotoPicker/PHMPhotoPicker-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double PHMPhotoPickerVersionNumber;
+FOUNDATION_EXPORT const unsigned char PHMPhotoPickerVersionString[];
+
